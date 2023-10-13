dfbc16ef841bc0ef97fb464e0e01c926455de196
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2023-10-13 18:51:09 -0400

    Initial Commit

---
+++ backend/Car-Insurance-Portal-Service/src/test/java/com/team10/carinsuranceportalservice/CarInsurancePortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team10.carinsuranceportalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInsurancePortalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
