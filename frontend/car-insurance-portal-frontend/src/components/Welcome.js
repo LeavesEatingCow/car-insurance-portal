679a1bf27aaa56b4d78a3482a5d3fa7482381b6c
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-10-15 19:05:09 -0400

    Finishing up Welcome page

--- frontend/car-insurance-portal-frontend/src/components/Welcome.js
+++ frontend/car-insurance-portal-frontend/src/components/Welcome.js
@@ -1,14 +1,21 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Welcome.css";
+import carLogo from "./images/white_car_image.png";
+import rapidinsureLogo from "./images/rapidinsurelogo1.png";
 
 function Welcome() {
   return (
     <div className="welcome">
-      <Link to="/Login">
-        <button className="login-btn">Log In</button>
-      </Link>
-      <Link to="/Signup">
-        <button className="signup-btn">Sign Up</button>
-      </Link>
+      <div className="content">
+        <div className="image-container">
+          <img src={carLogo} alt="White Car Logo" className="car-logo" />
+          <img src={rapidinsureLogo} alt="Logo" className="logo" />
+        </div>
+        <div className="buttons">
+          <button className="signup-btn">Sign Up</button>
+          <Link to="/Login" className="login-link">Log In</Link>
+        </div>
+      </div>
     </div>
   );
 }
