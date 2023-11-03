882013722415e708b9a3d4120e8b14f7b795ca85
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2023-11-03 17:34:48 -0400

    Add homepage, add route protection to check for JWT token

--- frontend/car-insurance-portal-frontend/src/App.js
+++ frontend/car-insurance-portal-frontend/src/App.js
@@ -2,7 +2,18 @@ import './App.css';
 import Welcome from "./components/Welcome";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes, useLocation} from "react-router-dom";
+import Homepage from "./components/Homepage";
+
+function PrivateRoute({ children, ...rest }) {
+  const location = useLocation();
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/" state={{ from: location }} />;
+  }
+  return children;
+}
 
 function App() {
   return(
@@ -11,6 +22,14 @@ function App() {
         <Route path="/" element={ <Welcome/> } />
         <Route path="/Login" element={ <Login/>} />
         <Route path="/Signup" element={ <Signup/>} />
+        <Route
+          path="/homepage"
+          element={
+            <PrivateRoute>
+              <Homepage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
