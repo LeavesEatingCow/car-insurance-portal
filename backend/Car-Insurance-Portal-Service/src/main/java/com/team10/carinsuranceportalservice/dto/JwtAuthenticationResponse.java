d8c14b15512098738d99234ffe3f8503f5cf505c
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2023-11-18 18:30:55 -0500

    Update JWT Authentication, return JWT Token in response body when in UserRegistrationController

---
+++ backend/Car-Insurance-Portal-Service/src/main/java/com/team10/carinsuranceportalservice/dto/JwtAuthenticationResponse.java
@@ -0,0 +1,20 @@
+package com.team10.carinsuranceportalservice.dto;
+
+public class JwtAuthenticationResponse {
+    private String token;
+
+    public JwtAuthenticationResponse() {
+    }
+
+    public JwtAuthenticationResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
