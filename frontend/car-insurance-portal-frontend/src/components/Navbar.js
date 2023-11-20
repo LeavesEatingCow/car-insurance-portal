d8f7cb2b0e7822d6873758a739aae58ee11ad2a5
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-11-20 11:06:33 -0500

    Filled out Welcome page

--- frontend/car-insurance-portal-frontend/src/components/Navbar.js
+++ frontend/car-insurance-portal-frontend/src/components/Navbar.js
@@ -17,9 +17,69 @@ import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Testimonials</a>
+        <a href="">Contact</a>
+        <a href="">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+        <button className="primary-button">Bookings Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/car-insurance-portal-frontend/src/components/Welcome.js
+++ frontend/car-insurance-portal-frontend/src/components/Welcome.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import Navbar from './Navbar'
+import BannerBackground from "../Assets/home-banner-background.png"
+import BannerImage from "../Assets/home-banner-image.png"
+import { FiArrowRight } from "react-icons/fi"
 
 const Welcome = () => {
   return (
     <div className='home-container'>
       <Navbar/>
+      <div className="home-banner-container">
+        <div className='home-bannerImage-container'>
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className='home-text-section'>
+          <h1 className="primary heading">
+            Header text
+          </h1>
+          <p className='primary-text'>
+            Paragraph text
+          </p>
+          <button className='secondary-button'>
+            Order Now <FiArrowRight />
+          </button>
+        </div>
+
+        <div className='home-image-container'>
+          <img src={BannerImage} alt=""/>
+        </div>
+      </div>
     </div>
   )
 }
