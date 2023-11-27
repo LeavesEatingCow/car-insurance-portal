82fa704e53b915faa7425d0dd927297265ac16ba
JonathanAlvarado34 <a.jonny34@gmail.com>
2023-11-27 15:21:03 -0500

    Added Tips page.

--- frontend/car-insurance-portal-frontend/src/components/About.js
+++ frontend/car-insurance-portal-frontend/src/components/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; //react
 import AboutBackground from "../Assets/about-background.png";
 import AboutBackgroundImage from "../Assets/about-background-image.png";
 import { BsFillPlayCircleFill } from "react-icons/bs";
